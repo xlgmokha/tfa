63fa2bebe970fe89d07aec3fa3f799f7b5c88f9f
mo khan <mo@mokhan.ca>
2014-07-25 14:02:32 -0600

    add spec for reading out the secret.

---
+++ spec/lib/show_command_spec.rb
@@ -0,0 +1,16 @@
+module In
+  describe ShowCommand do
+    subject { ShowCommand.new(storage) }
+    let(:storage) { PStore.new(Tempfile.new('blah').path) }
+
+    it "retrieves the secret associated with the key given" do
+      secret = SecureRandom.uuid
+      storage.transaction do
+        storage['production'] = secret
+      end
+
+      result = subject.run(['production'])
+      expect(result).to eql(secret)
+    end
+  end
+end
