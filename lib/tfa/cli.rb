36657da56ba6896ff1196220c42380de1bb8fb59
mo khan <mo@mokhan.ca>
2014-09-26 14:47:12 -0600

    write to stdout in the bin/tfa file.

--- lib/tfa/cli.rb
+++ lib/tfa/cli.rb
@@ -7,27 +7,23 @@ module TFA
 
     desc "add NAME SECRET", "add a new secret to the database"
     def add(name, secret)
-      display AddCommand.new(storage).run([name, secret])
+      AddCommand.new(storage).run([name, secret])
     end
 
     desc "show NAME", "shows the secret for the given key"
     def show(name)
-      display ShowCommand.new(storage).run([name])
+      ShowCommand.new(storage).run([name])
     end
 
     desc "totp NAME", "generate a Time based One Time Password"
     def totp(name)
-      display TotpCommand.new(storage).run([name])
+      TotpCommand.new(storage).run([name])
     end
 
     private
 
     def storage
       @storage ||= Storage.new(filename: options[:filename] || 'tfa')
-    end
-
-    def display(message)
-      puts message
     end
   end
 end
